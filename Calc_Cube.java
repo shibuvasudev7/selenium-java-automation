5f2f8ddfc3b05b407351845ff8febc10f8f96ef3
Shibu Vasudev <93815520+shibuvasudev7@users.noreply.github.com>
2026-09-16 11:06:16 +0530

    Add Calc_Cube.java
    (renamed to avoid conflict with assesment/Calc.java)

---
+++ Calc_Cube.java
@@ -0,0 +1,13 @@
+package oops;
+
+public class Calc {
+static int cube(int x) {
+	return x*x*x;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int result = Calc.cube(10);
+System.out.println(result);
+	}
+
+}
